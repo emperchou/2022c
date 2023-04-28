c4fab75f1ecd91a3e3ce13d25c22110e30718904
emperchou <ggggwpchou@gmail.com>
2023-04-28 11:53:55 +0800

    Week11

---
+++ week11/week11-1.cpp
@@ -0,0 +1,26 @@
+int lastStoneWeight(int* stones, int N)
+{
+    while(1){
+        int a=stones[0],ai=0;
+        for(int i=0;i<N;i++){
+            if(stones[i]>a){
+                a = stones[i];
+                ai=i;
+            }
+        }
+        stones[ai] = 0;
+        int b=stones[0],bi=0;
+        for(int i=0;i<N;i++){
+            if(stones[i]>b){
+                b = stones[i];
+                bi=i;
+            }
+        }
+        stones[bi]=0;
+        if(a==0 && b==0)return 0;
+        if(a!=0 && b==0)return a;
+        stones[ai] = a-b;
+    }
+    return 0;
+
+}
